ee75397c2af58483f6963144ce9f1a69ec4f46be
nomanzawad <nomanzawad@gmail.com>
2024-09-27 06:02:42 +0000

    developing nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter } from "react-router-dom"
 import {About ,Contact, Experience,Feedbacks,Hero,Navbar,Tech,Works, StarsCanvas} from "./components"
+import { useState } from "react"
 const App =() => {
-
+  const [active, setActive] = useState("");
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import {useEffect , useState} from 'react';
 import { Link } from 'react-router-dom';
 import  {styles} from '../styles';
-import {navLinks} from '../utils/constants';
+import {navLinks} from '../constants';
 import { logo,menu,close } from '../assets';
+import { link } from 'framer-motion/client';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -14,7 +15,15 @@ const Navbar = () => {
           window.scrollTo(0,0);
         }}>
         <img src={logo} alt='logo' className='w-9 h-9 object-contain'/>
+        <p className='text-white cursor-pointer text-[18px] font-bold'>Noman <span className='sm:block hidden'>| Web Developer</span></p>
         </Link>
+        <ul className='linst-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((links)=>{
+            <li key={links.id} className={`${active ===links.title ? "text-white": "text-secondary"}`}>
+              <a href={`${links.id}`}>{links.title}</a>
+            </li>
+          })}
+        </ul>
       </div>
     </nav>
   )
